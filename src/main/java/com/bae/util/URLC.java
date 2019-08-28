fe7948a241fade9a945e5b4301bb74811b6f8e53
ayshamarty <50907372+ayshamarty@users.noreply.github.com>
2019-08-28 10:00:15 +0100

    Update URLC.java

--- src/main/java/com/bae/util/URLC.java
+++ src/main/java/com/bae/util/URLC.java
@@ -3,16 +3,16 @@ package com.bae.util;
 public class URLC {
 
 //		localhost
-		public static final String CITIZEN_URL = "http://localhost:8081";
-		public static final String FINANCE_URL = "http://localhost:8083";
-		public static final String MOBILE_URL = "http://localhost:8084";
-		public static final String VEHICLE_URL = "http://localhost:8082";
+//		public static final String CITIZEN_URL = "http://localhost:8081";
+//		public static final String FINANCE_URL = "http://localhost:8083";
+//		public static final String MOBILE_URL = "http://localhost:8084";
+//		public static final String VEHICLE_URL = "http://localhost:8082";
 		
 //		swarm
-//		public static final String CITIZEN_URL = "citizen;
-//		public static final String FINANCE_URL = "finance;
-//		public static final String MOBILE_URL = "mobile;
-//		public static final String VEHICLE_URL = "anpr";
+		public static final String CITIZEN_URL = "http://core:8081";
+		public static final String FINANCE_URL = "http://core:8083";
+		public static final String MOBILE_URL = "http://core:8084";
+		public static final String VEHICLE_URL = "http://core:8082";
 		
 //		kubes
 //		public static final String CITIZEN_URL = "";
