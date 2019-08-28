71a7735f23680f72912554bbc6e2531a730b7c79
ayshamarty <50907372+ayshamarty@users.noreply.github.com>
2019-08-28 12:35:23 +0100

    Update URLC.java

--- src/main/java/com/bae/util/URLC.java
+++ src/main/java/com/bae/util/URLC.java
@@ -9,10 +9,10 @@ public class URLC {
 //		public static final String VEHICLE_URL = "http://localhost:8082";
 		
 //		swarm
-		public static final String CITIZEN_URL = "http://citizen:8081";
-		public static final String FINANCE_URL = "http://finance:8083";
-		public static final String MOBILE_URL = "http://mobile:8084";
-		public static final String VEHICLE_URL = "http://anpr:8082";
+		public static final String CITIZEN_URL = "/citizen";
+		public static final String FINANCE_URL = "/finance";
+		public static final String MOBILE_URL = "/mobile";
+		public static final String VEHICLE_URL = "/anpr";
 		
 //		kubes
 //		public static final String CITIZEN_URL = "";
